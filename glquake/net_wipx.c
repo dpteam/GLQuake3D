59e56234a96d790edc6002ea0aafe9f85b29a699
DartPower <SIMPSON-NEKEK@mail.ru>
2020-09-06 21:13:58 +0300

    Update net_wipx.c

--- glquake/net_wipx.c
+++ glquake/net_wipx.c
@@ -244,9 +244,9 @@ int WIPX_Read (int handle, byte *buf, int len, struct qsockaddr *addr)
 	ret = precvfrom (socket, packetBuffer, len+4, 0, (struct sockaddr *)addr, &addrlen);
 	if (ret == -1)
 	{
-		int errno = pWSAGetLastError();
+		int qerrno = pWSAGetLastError();
 
-		if (errno == WSAEWOULDBLOCK || errno == WSAECONNREFUSED)
+		if (qerrno == WSAEWOULDBLOCK || qerrno == WSAECONNREFUSED)
 			return 0;
 
 	}
